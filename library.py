c99d03c06f145cfbe3eb50713760669dc45a624f
ramesh-exalogic <ramesh.kumar@exalogic.co>
2024-12-02 15:51:17 +0530

    added the choices for the books

---
+++ library.py
@@ -0,0 +1,9 @@
+while True:
+    print("Display Available book please enter 1")
+    print("Display all books please enter 2")
+    print("book edukanum please enetr 3")
+    print("book ah return pannanum please enetr 4")
+    print("Quit Please enter 5")
+    value = int(input("please entter the choice"))
+    if value == 5:
+        break
